559b4f8f8eb5a2e2f992f6fd01a855a290c8f56b
Raviraja <raviraja.ghanta@gmail.com>
2020-09-16 21:27:18 +0530

    overwritten default servemux

--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"os/signal"
 	"log"
 	"net/http"
+	"os"
+	"time"
+	"github.com/graviraja/go-sample-microservice/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world")
-	})
-
-	http.ListenAndServe(":9090", nil)
+	sig := <-sigChan
+	l.Println("Recevied terminate, graceful shutdown", sig)
+	
+	tc, _ := context.WithTimeout(context.Background(), 30 * time.Second)
+	s.Shutdown(tc)
 }
